200e806f486bc2ec8213ea99843aadd37afd25fe
Jumong <lomojiki@gmail.com>
2024-03-16 09:54:31 +0900

    wip. thymleaf ssr version, csr version. not gonna use ssr version view route controller

--- view/src/main/java/com/example/view/RouteController.java
+++ view/src/main/java/com/example/view/RouteController.java
@@ -1,9 +1,14 @@
 package com.example.view;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @GetMapping("/route")
-public String showRoute(Model model) {
+public String showRoute((@RequestParam("startLat") Double startLat,
+                       @RequestParam("startLon") Double startLon,
+                       @RequestParam("destLat") Double destLat,
+                       @RequestParam("destLon") Double destLon,
+                       Model model) {
     // graphhopper에서 좌표 리스트를 가져옵니다.
     List<GHPoint> ghPoints = graphhopperService.getRoute(...);
     List<double[]> coordinates = ghPoints.stream()
