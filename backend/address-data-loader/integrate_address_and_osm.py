b041c5b1178d7821c78509241a25fc251777c4f6
Jumong <lomojiki@gmail.com>
2024-04-12 12:08:02 +0900

    address-search -> address-data-loader folder name changed. wip. elastic query building

---
+++ backend/address-data-loader/integrate_address_and_osm.py
@@ -0,0 +1,77 @@
+from elasticsearch import Elasticsearch, helpers
+import math
+
+es = Elasticsearch(['http://localhost:9200'])
+
+# 주소 인덱스의 총 문서 수 가져오기
+address_count = es.count(index="address")["count"]
+batch_size = 100
+
+# 주소 인덱스에서 postal_code 가져오기
+address_query = {
+    "_source": ["postal_code"],
+    "query": {
+        "exists": {
+            "field": "postal_code"
+        }
+    }
+}
+
+# osm 인덱스에서 postal_code와 osmid를 가져옴
+osm_query = {
+    "_source": ["postal_code", "osmid"],
+    "query": {
+        "exists": {
+            "field": "postal_code"
+        }
+    }
+}
+
+# osm 인덱스에서 postal_code와 osmid를 가져와 딕셔너리로 변환
+osm_codes = {}
+scan_result = helpers.scan(es, index="osm", query=osm_query, size=1000)
+for hit in scan_result:
+    print(hit)
+    postal_code = hit["_source"]["postal_code"]
+    osmid = hit["_source"]["osmid"]
+    if postal_code in osm_codes:
+        osm_codes[postal_code].append(osmid)
+    else:
+        osm_codes[postal_code] = [osmid]
+
+
+# address 인덱스에 osmid 필드 추가
+res = es.search(index="address", body=address_query, scroll='2m')
+sid = res['_scroll_id']
+total_hits = res['hits']['total']['value']
+hits = res['hits']['hits']
+print("total number of document (this is not per scroll) hits: {}".format(total_hits))
+print('hits result: {}'.format(len(hits)))
+
+while len(hits) > 0:
+    actions = []
+    for hit in hits:
+        postal_code = hit["_source"]["postal_code"]
+        if postal_code in osm_codes:
+            osmids = osm_codes[postal_code]
+            action = {
+                "_op_type": "update",
+                "_index": "address",
+                "_id": hit["_id"],
+                "script": {
+                    "source": "ctx._source.osmids = params.osmids",
+                    "params": {
+                        "osmids": osmids
+                    }
+                }
+            }
+            actions.append(action)
+
+    helpers.bulk(es, actions, index="address", refresh=True, request_timeout=30)
+    print(f"Processed {len(actions)} documents")
+
+    res = es.scroll(scroll_id=sid, scroll='2m')
+    break
+
+# 스크롤 컨텍스트 삭제
+es.clear_scroll(scroll_id=sid)
---
+++ backend/address-data-loader/save_osm_elastic_search.py
@@ -0,0 +1,144 @@
+import osmium as osm
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk, BulkIndexError
+
+INDEX_NAME = 'osm'
+bulk_size = 100
+DEBUG_COUNT = 2
+CREATE_INDEX_FLAG = True
+
+# osm data tags
+# 건물 관련 태그:
+# building: 건물의 존재를 나타냅니다.
+# building:use: 건물의 용도를 나타냅니다. (예: residential, commercial, industrial 등)
+# building:levels: 건물의 층수를 나타냅니다.
+# 도로 관련 태그:
+# highway: 도로의 종류를 나타냅니다. (예: motorway, primary, residential 등)
+# name: 도로의 이름을 나타냅니다.
+# oneway: 일방통행 여부를 나타냅니다.
+# maxspeed: 도로의 최대 속도 제한을 나타냅니다.
+# 토지 이용 태그:
+# landuse: 토지의 용도를 나타냅니다. (예: residential, commercial, industrial, agricultural 등)
+# natural: 자연적인 특징을 나타냅니다. (예: wood, water, grassland 등)
+# 편의 시설 태그:
+# amenity: 편의 시설의 종류를 나타냅니다. (예: school, hospital, restaurant, bank 등)
+# shop: 상점의 종류를 나타냅니다. (예: supermarket, bakery, clothing 등)
+# 기타 태그:
+# name: 지역, 건물, 시설 등의 이름을 나타냅니다.
+# operator: 시설이나 서비스의 운영자를 나타냅니다.
+# opening_hours: 시설의 운영 시간을 나타냅니다.
+
+mapping = {
+    "properties": {
+        "housenumber": {
+            "type": "keyword"
+        },
+        "street": {
+            "type": "text"
+        },
+        "city": {
+            "type": "keyword"
+        },
+        "postal_code": {
+            "type": "keyword"
+        },
+        "lat": {
+            "type": "float"
+        },
+        "lon": {
+            "type": "float"
+        },
+        "osmid": {
+            "type": "long"
+        }
+    }
+}
+class AddressHandler(osm.SimpleHandler):
+    def __init__(self, es):
+        osm.SimpleHandler.__init__(self)
+        self.row_list = []
+        self.es = es
+        self.bulk_size = bulk_size
+
+        self.es.index(index=INDEX_NAME, body={
+            'mapping': {
+                'properties': mapping
+            }
+        })
+
+    def node(self, n):
+        global DEBUG_COUNT
+        if 'addr:housenumber' in n.tags and 'addr:street' in n.tags:
+            address = {
+                'housenumber': n.tags['addr:housenumber'],
+                'street': n.tags['addr:street'],
+                'city': n.tags.get('addr:city', ''),
+                'postal_code': n.tags.get('addr:postcode', ''),
+                'lat': n.location.lat,
+                'lon': n.location.lon,
+                'osmid': n.id
+            }
+            self.row_list.append(address)
+
+
+    def update(self):
+        batch = list()
+        for row in self.row_list:
+
+            action = {
+                '_index': INDEX_NAME,
+                '_source': row
+            }
+            batch.append(action)
+            if len(self.addresses) >= self.bulk_size:
+                try:
+                    bulk(self.es, batch)
+                except BulkIndexError as e:
+                    print("Error indexing documents:")
+                    for error in e.errors:
+                        print(error)
+                batch = list()
+
+
+def create_index(osm_file, es):
+    handler = AddressHandler(es)
+    handler.apply_file(osm_file)
+    if handler.addresses:
+        handler.bulk_update()
+
+
+
+
+
+# Elasticsearch 연결 설정
+es = Elasticsearch(['http://localhost:9200'])
+
+
+# 사용 예시
+osm_file = 'seoul-non-military.osm.pbf'
+
+if CREATE_INDEX_FLAG:
+    create_index(osm_file, es)
+
+
+search_results = es.search(index=INDEX_NAME, body={
+        'query': {
+            'bool': {
+                'filter': [
+                    {'exists': {'field': 'postal_code'}},
+                    {'exists': {'field': 'housenumber'}}
+                ]
+            }
+        },
+        'from': 0,
+        'size': 50
+    })
+
+# [hit['_source'] for hit in search_results['hits']['hits']]
+if search_results:
+    for addr in search_results:
+        print(f"주소: {addr['street']} {addr['housenumber']}, {addr['city']} {addr['postcode']}")
+        print(f"위도: {addr['lat']}, 경도: {addr['lon']}")
+        print("---")
+else:
+    print("검색 결과가 없습니다.")
